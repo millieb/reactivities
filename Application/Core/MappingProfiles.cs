c9267ae0601f3fc85be512a765e77a8620047bd6
millieb <mily.brit@gmail.com>
2023-11-06 13:45:33 -0700

    End of Section 4 - CRUD App using CORS and Mediator Pattern

---
+++ Application/Core/MappingProfiles.cs
@@ -0,0 +1,15 @@
+
+
+using System.Diagnostics;
+using AutoMapper;
+
+namespace Application.Core
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Activity, Activity>();
+        }
+    }
+}
